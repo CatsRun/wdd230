0a8a2ae303c6556cc44cd6e0ba5860f583b8b396
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-03-11 23:05:23 -0400

    fix css

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,19 +1,4 @@
-// getDate
-// document.getElementById("year").innerHTML = (new Date().getFullYear());
-
-// date larst modified
-// copyright year
-
-// const options = {year:'numeric'};
-// document.getElementById('currentYear').textContent = new Date().toLocaleDateString(options );
-// 
-
-// console.log(currentyear.getFullYear());
-// document.getElementById(currentyear).textContent = new Date().toLocaleDateString(currentyear.getFullYear());
-
-//prints current year
-function printYear(){
-    
+function printYear(){    
     const currentYear = new Date();
     return currentYear.getFullYear();
 }
@@ -25,9 +10,6 @@ function printLastModefied(){
     let month = lastModefied.getMonth();
     let day = lastModefied.getDate(); 
     let year = lastModefied.getFullYear();
-    let hour = lastModefied.getHours();
-    let minutes = lastModefied.getMinutes();
-    let seconds = lastModefied.getSeconds();
     let timeFormated = lastModefied.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12:true});
     const dateFormated = month + "/" + day + "/" + year + " " + timeFormated;
     
@@ -35,6 +17,3 @@ function printLastModefied(){
 }
 
 document.getElementById("lastModified").innerHTML = printLastModefied();
-
-console.log(printLastModefied());
-// console.log(Date());
