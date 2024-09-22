3be95ed5b1d0de3fbc850dd74e6ed362d588cd49
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-09-21 23:54:55 -0400

    basic code

---
+++ scripts/getdates.js
@@ -0,0 +1,19 @@
+function printYear(){    
+    const currentYear = new Date();
+    return currentYear.getFullYear();
+}
+
+document.getElementById("currentYear").innerHTML = printYear();
+
+function printLastModefied(){
+    const lastModefied = new Date();
+    let month = lastModefied.getMonth();
+    let day = lastModefied.getDate(); 
+    let year = lastModefied.getFullYear();
+    let timeFormated = lastModefied.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12:true});
+    const dateFormated = month + "/" + day + "/" + year + " " + timeFormated;
+    
+    return dateFormated;
+}
+
+document.getElementById("lastModified").innerHTML = printLastModefied();
