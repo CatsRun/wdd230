266e2d6b6b3eaab56052b5cf9f68fe5974e5ba09
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-10-26 12:21:07 -0400

    dark mode works

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,14 +1,17 @@
-// const modeButton = document.querySelector("#mode");
-// const main = document.querySelector("main");
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
 
-// modeButton.addEventListener("click", () => {
-// 	if (modeButton.textContent.includes("🕶️")) {
-// 		main.style.background = "#000";
-// 		main.style.color = "#fff";
-// 		modeButton.textContent = "🔆";
-// 	} else {
-// 		main.style.background = "#eee";
-// 		main.style.color = "#000";
-// 		modeButton.textContent = "🕶️";
-// 	}
-// });
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("😎")) {
+    main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "😀";
+    body.style.background = "#000";
+	} else {
+		main.style.background = "beige";
+		main.style.color = "#000";
+		modeButton.textContent = "😎";
+        body.style.background = "beige";
+	}
+});
