81e5650333d8bc2fa5154943bf1f638d0834102e
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-10-26 14:40:03 -0400

    add herolable

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,5 +1,6 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+// const herolable = document.querySelector('.herolable');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
