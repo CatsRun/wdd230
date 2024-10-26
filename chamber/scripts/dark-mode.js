1e892dd9f0df2b8a734661f0048cb00d42dba62d
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-10-26 13:05:27 -0400

    fix font color

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -1,17 +1,20 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
+const header = document.querySelector('header')
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("😎")) {
-    main.style.background = "#000";
+        main.style.background = "#000";
 		main.style.color = "#fff";
 		modeButton.textContent = "😀";
-    body.style.background = "#000";
+        body.style.background = "#000";
+        header.style.color = "#000";
 	} else {
 		main.style.background = "beige";
 		main.style.color = "#000";
 		modeButton.textContent = "😎";
         body.style.background = "beige";
+        header.style.color = "#fff";
 	}
 });
