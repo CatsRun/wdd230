1bcffbf028355f80898745f979c8612fec7c0b83
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-10-27 01:18:24 -0400

    add windchill chamber

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,28 @@
+/*
+         * Defining Table
+         * Input(s): temperature, wind speed
+         * Processing: calculate the wind chill factor temperature in Fahrenheit
+         * Output(s): windchill in Fahrenheit
+         */
+function doInputOutput(){
+    let tempF = parseFloat(document.getElementById('Temperature').value);
+    let speed = parseFloat(document.getElementById('Wind-Speed').value);
+    
+    windChill(tempF, speed);
+    
+    output = windChill(tempF, speed);
+    
+    document.getElementById("output").innerHTML = output.toFixed(1);
+}
+    
+function windChill(tempF, speed){
+    let na = 'N/A';
+    if (speed >= 3 && tempF < 50) {
+        return 35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16;                          
+        }
+    else {
+        // output is a number. I need to change it to a string for this to work
+        return 0;
+    }
+
+}
