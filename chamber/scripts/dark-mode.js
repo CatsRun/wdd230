8ca681d3ba70ed864486a0b5c36eecc37a7b3fd2
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-10-27 01:27:14 -0400

    fix color again

--- chamber/scripts/dark-mode.js
+++ chamber/scripts/dark-mode.js
@@ -2,6 +2,7 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const body = document.querySelector("body");
 const header = document.querySelector('header')
+const herolable = document.querySelector('.herolable')
 
 modeButton.addEventListener("click", () => {
     if (modeButton.textContent.includes("😎")) {
@@ -10,11 +11,13 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "😀";
         body.style.background = "#000";
         header.style.color = "#000";
+		herolable.style.color = "#000";
 	} else {
 		main.style.background = "beige";
 		main.style.color = "#000";
 		modeButton.textContent = "😎";
         body.style.background = "beige";
         header.style.color = "#fff";
+		herolable.style.color = "#000";
 	}
 });
