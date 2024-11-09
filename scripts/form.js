ce3f4d411f08b944d751bd077447aa216eed2bc3
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-11-09 11:54:04 -0500

    form update

--- scripts/form.js
+++ scripts/form.js
@@ -9,23 +9,23 @@ function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
 
-// const kp1 = document.querySelector("#keyphrase");
-// const kp2 = document.querySelector("#keyphrase2");
-// const message = document.querySelector("#formmessage");
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
 
-// kp2.addEventListener("focusout", checkSame);
+kp2.addEventListener("focusout", checkSame);
 
-// // This should be refactored.
-// function checkSame() {
-// 	if (kp1.value !== kp2.value) {
-// 		message.textContent = "❗Key Phrases DO NOT MATCH!";
-// 		message.style.visibility = "show";
-// 		kp2.style.backgroundColor = "#fff0f3";
-// 		kp2.value = "";
-// 		kp2.focus();
-// 	} else {
-// 		message.style.display = "none";
-// 		kp2.style.backgroundColor = "#fff";
-// 		kp2.style.color = "#000";
-// 	}
-// }
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
