fba29e428ec8507b1fd6fe3828ba3e26e284a87d
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-11-20 10:12:38 -0500

    prophets css

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -16,16 +16,22 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
+        let dob = document.createElement('h3');
+        let place = document.createElement('h3');
         let portrait = document.createElement('img');
 
         
         portrait.setAttribute('src', prophet.imageurl);
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+        place.textContent = `Place of Birth: ${prophet.birthplace}`;
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
         card.appendChild(fullName); 
+        card.appendChild(dob);
+        card.appendChild(place);
         card.appendChild(portrait);
     
         cards.appendChild(card);
