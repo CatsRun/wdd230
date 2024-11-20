53e34bf21ff12a66fe0277ae57efd5149b0c38ff
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-11-20 09:40:55 -0500

    update prophets js

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+// fetches the data from the JSON file
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // to test table data
+    console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        
+        portrait.setAttribute('src', prophet.imageurl);
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName); 
+        card.appendChild(portrait);
+    
+        cards.appendChild(card);
+        
+    });
+}
+
+
+
+// where does 'prophets' come from? It comes frim the JSON file. 
