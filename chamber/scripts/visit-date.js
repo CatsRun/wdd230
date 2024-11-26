dd7436efa5377bf49e5df904751c99dd8490cf48
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-11-25 21:38:46 -0500

    update visit-date

--- chamber/scripts/visit-date.js
+++ chamber/scripts/visit-date.js
@@ -1,40 +1,41 @@
 // chamber.discovery (visits card)
-// stores visit counter locally
+// stores visit date locally
 const visitsDisplay = document.querySelector("#visits");
-// let today = Date.now();
 const currentDate = new Date();
 const lastVisit = localStorage.getItem('lastVisit');
 localStorage.setItem('lastVisit', currentDate.getTime());
 // console.log(currentDate);
 // console.log(lastVisit);
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-// firstdate needs to reference the date in local storage***
-// const firstDate = new Date(2008, 1, 12);
+const firstDate = new Date(2024, 10, 25); //change this with lastVisted to test
 const diffDays = Math.round(Math.abs((currentDate - lastVisit) / oneDay));
-// console.log(diffDays);
+console.log(diffDays);
+console.log(currentDate);
 
 
-if (diffDays == 0 ){ 
+// if (diffDays == 0 ){ 
 	// const lastVisit = new Date(lastVisit);
     // if ( Date.now() == today ){
-		if (diffDays < 1) {
-		visitsDisplay.textContent = `Back so soon! Awesome!`;
-	}
-	
-	else   {
-		// amount of time between visits is less than a day
-		visitsDisplay.textContent =`You last visited ${diffDays} days ago.`
-	}
-}	 
+	if (diffDays <= 1 ) {
+	visitsDisplay.textContent = `Back so soon! Awesome!`;
+}
+
+	else if (diffDays > 1 && diffDays < 2)   {
+	// amount of time between visits is less than a day
+	visitsDisplay.textContent =`You last visited ${diffDays} day ago.`
+}
+else if (diffDays >= 2)  {
+	// amount of time between visits is less than a day
+	visitsDisplay.textContent =`You last visited ${diffDays} days ago.`
+}
+
+// }	 
 
 else {
 	// first visit
-		visitsDisplay.textContent =`Welcome! Let us know if you have any questions.`;
-		// console.log('no');
+		visitsDisplay.textContent =`Welcome! Let us know if you have any questions.`;		
 }
 
-// numVisits++;
-// localStorage.setItem("numVisits-ls", numVisits);
 // console.log(localStorage['lastVisit']);
 var myDate2 = new Date(parseInt(localStorage['lastVisit'], 10)); 
 // console.log("|"+myDate2+"|");
