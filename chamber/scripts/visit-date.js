a925e2b254a84bd5eb741ac36edfd21fdee121f8
CatsRun <97203917+CatsRun@users.noreply.github.com>
2024-11-25 19:43:46 -0500

    fix visit-date

--- chamber/scripts/visit-date.js
+++ chamber/scripts/visit-date.js
@@ -1,19 +1,20 @@
+// chamber.discovery (visits card)
 // stores visit counter locally
 const visitsDisplay = document.querySelector("#visits");
-// let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 // let today = Date.now();
 const currentDate = new Date();
 const lastVisit = localStorage.getItem('lastVisit');
 localStorage.setItem('lastVisit', currentDate.getTime());
-console.log(currentDate);
-console.log(lastVisit);
+// console.log(currentDate);
+// console.log(lastVisit);
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-const firstDate = new Date(2008, 1, 12);
-const diffDays = Math.round(Math.abs((firstDate - lastVisit) / oneDay));
-console.log(diffDays);
+// firstdate needs to reference the date in local storage***
+// const firstDate = new Date(2008, 1, 12);
+const diffDays = Math.round(Math.abs((currentDate - lastVisit) / oneDay));
+// console.log(diffDays);
 
-// if (numVisits !== 0) {
-if (diffDays !== 0 ){ 
+
+if (diffDays == 0 ){ 
 	// const lastVisit = new Date(lastVisit);
     // if ( Date.now() == today ){
 		if (diffDays < 1) {
@@ -21,24 +22,24 @@ if (diffDays !== 0 ){
 	}
 	
 	else   {
+		// amount of time between visits is less than a day
 		visitsDisplay.textContent =`You last visited ${diffDays} days ago.`
 	}
 }	 
 
 else {
-		visitsDisplay.textContent =`Welcome! Let us know if you have any questions`;
-		console.log(no);
+	// first visit
+		visitsDisplay.textContent =`Welcome! Let us know if you have any questions.`;
+		// console.log('no');
 }
 
 // numVisits++;
 // localStorage.setItem("numVisits-ls", numVisits);
-console.log(localStorage['lastVisit']);
+// console.log(localStorage['lastVisit']);
 var myDate2 = new Date(parseInt(localStorage['lastVisit'], 10)); 
-console.log("|"+myDate2+"|");
+// console.log("|"+myDate2+"|");
 localStorage['lastVisit'] = ''+myDate2.getTime();
 
 
 
-console.log(myDate2);
 
-// I need to save the last visited date and then compare that to DateNow(). 
